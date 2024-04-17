b17eec192fcc7755b3929ada26aa6c8e6a513e9d
hipSalman <salman@hyppe.id>
2024-04-17 09:10:31 +0700

    interactive untuk temppost

--- src/content/disqus/newpost/newpost.module.ts
+++ src/content/disqus/newpost/newpost.module.ts
@@ -2,18 +2,20 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Newpost, NewpostSchema } from '..//../disqus/newpost/schemas/newpost.schema';
+import { tempposts2, temppostsSchema } from './schemas/temppost.schema';
 import { NewpostController } from './newpost.controller';
 import { NewpostService } from './newpost.service';
 import { UtilsModule } from '../../../utils/utils.module';
 import { SeaweedfsModule } from '../../../stream/seaweedfs/seaweedfs.module';
+import { temppostDISCUSS } from './temppost.service';
 @Module({
 
     imports: [
         ConfigModule.forRoot(),UtilsModule,SeaweedfsModule,
-        MongooseModule.forFeature([{ name: Newpost.name, schema: NewpostSchema }], 'SERVER_FULL')
+        MongooseModule.forFeature([{ name: Newpost.name, schema: NewpostSchema }, { name: tempposts2.name, schema: temppostsSchema }], 'SERVER_FULL')
     ],
     controllers: [NewpostController],
-    providers: [NewpostService],
-    exports: [NewpostService],
+    providers: [NewpostService, temppostDISCUSS],
+    exports: [NewpostService, temppostDISCUSS],
 })
 export class NewpostModule { }
