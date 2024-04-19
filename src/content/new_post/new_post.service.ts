5a569a6a926eefa5329079a0d8176d53e37c3765
kusnur629 <kusnur9@gmail.com>
2024-04-19 22:25:42 +0700

    update

--- src/content/new_post/new_post.service.ts
+++ src/content/new_post/new_post.service.ts
@@ -59443,7 +59443,7 @@ async findbylikemail(email:string,postID:string): Promise<newPosts[]> {
                   '$dateToString': {
                       format: '%Y-%m-%d %H:%M:%S',
                       date: {
-                          '$add': [new Date(), - 6048000000]
+                          '$add': [new Date(), - 3024000000]
                       }
                   }
               }
