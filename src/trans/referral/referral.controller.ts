432a1bef82ae7f105cbcbcaca7fa7b097fd48808
hipSalman <salman@hyppe.id>
2024-04-25 11:38:12 +0700

    perbaikan listing referal

--- src/trans/referral/referral.controller.ts
+++ src/trans/referral/referral.controller.ts
@@ -32,7 +32,7 @@ export class ReferralController {
   @Post('/list')
   async list(@Req() request: Request) {
     var request_json = JSON.parse(JSON.stringify(request.body));
-    let skip = (request_json.page > 0 ? (request_json.page - 1) : 0) * request_json.limit;
+    let skip = (request_json.page > 0 ? request_json.page : 0) * request_json.limit;
     return this.referralService.listAll(request_json.email, request_json.from, request_json.to, request_json.jenis, request_json.username, skip, request_json.limit);
   }
 
