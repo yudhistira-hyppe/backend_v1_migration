221f18ca1c290817262ddf59f18dd545a479b7d0
hipSalman <salman@hyppe.id>
2024-05-17 14:53:01 +0700

    update output transaction setelah posting

--- src/content/new_post/new_post.module.ts
+++ src/content/new_post/new_post.module.ts
@@ -15,6 +15,7 @@ import { PostsModule } from '../posts/posts.module';
 import { GetusercontentsModule } from 'src/trans/getusercontents/getusercontents.module';
 import { MediamusicModule } from '../mediamusic/mediamusic.module';
 import { SettingsModule } from 'src/trans/settings/settings.module';
+import { Settings2Module } from 'src/trans/settings2/settings2.module';
 import { MediastikerModule } from '../mediastiker/mediastiker.module';
 import { NewPostContentService } from './new_postcontent.service';
 import { InterestsModule } from 'src/infra/interests/interests.module';
@@ -53,6 +54,7 @@ import { MonetizationModule } from 'src/trans/monetization/monetization.module';
     PostchallengeModule,
     UserchallengesModule,
     SettingsModule,
+    Settings2Module,
     ContenteventsModule,
     UserbasicnewModule,
     MediavideosModule,
