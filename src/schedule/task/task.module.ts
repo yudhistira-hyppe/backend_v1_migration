a45882f382d5439d22069bb799a8a36accf23d83
sukmahyppe <sukma@hyppe.id>
2024-05-22 14:15:18 +0700

    update

--- src/schedule/task/task.module.ts
+++ src/schedule/task/task.module.ts
@@ -3,19 +3,15 @@ import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
 import { ScheduleModule } from '@nestjs/schedule';
-import { UtilsModule } from '../../utils/utils.module';
-import { UserticketsModule } from '../../trans/usertickets/usertickets.module';
-import { UserbasicsModule } from '../../trans/userbasics/userbasics.module';
-import { PostsModule } from '../../content/posts/posts.module';
 import { ChallengeModule } from 'src/trans/challenge/challenge.module';
 import { TransactionsModule } from 'src/trans/transactions/transactions.module';
 import { AdsModule } from 'src/trans/adsv2/ads/ads.module';
 import { NewPostModule } from 'src/content/new_post/new_post.module';
 import { PosttaskModule } from '../../content/posttask/posttask.module';
-import { UserbasicnewModule } from 'src/trans/userbasicnew/userbasicnew.module';
+import { MediastreamingModule } from 'src/content/mediastreaming/mediastreaming.module';
 @Module({
   imports: [
-    UserbasicnewModule,
+    MediastreamingModule,
     PosttaskModule,
     NewPostModule,
       AdsModule,
