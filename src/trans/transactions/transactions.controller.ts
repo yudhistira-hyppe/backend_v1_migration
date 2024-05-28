f65a57821694a50683702e171e4d44dd9a63a539
haris-dwi-prakoso <harisdwi.prakoso@gmail.com>
2024-05-28 09:12:00 +0700

    update withdrawtransactiondetail minimum amount check
    Signed-off-by: haris-dwi-prakoso <harisdwi.prakoso@gmail.com>

--- src/trans/transactions/transactions.controller.ts
+++ src/trans/transactions/transactions.controller.ts
@@ -20287,7 +20287,7 @@ export class TransactionsController {
     @UseGuards(JwtAuthGuard)
     async withdrawDetail(@Req() request: Request, @Headers() headers): Promise<any> {
         var timestamps_start = await this.utilsService.getDateTimeString();
-        var fullurl = headers.host + '/api/transactions/coinpurchasedetail';
+        var fullurl = headers.host + '/api/transactions/withdrawtransactiondetail';
         var token = headers['x-auth-token'];
         var auth = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
         var setemail = auth.email;
@@ -20319,10 +20319,10 @@ export class TransactionsController {
         var bankCharge = await this.settingsService.findOne(process.env.ID_SETTING_COST_BUY_COIN);
         var totalAmount = amount - (convertFee + Number(bankCharge.value));
         var minAmount = await this.settingsService.findOneByJenis("SaldoMinimumPenarikan");
-        if (totalAmount < Number(minAmount.value)) {
+        if (amount < Number(minAmount.value)) {
             var timestamps_end = await this.utilsService.getDateTimeString();
             this.logapiSS.create2(fullurl, timestamps_start, timestamps_end, setemail, null, null, request_json);
-            await this.errorHandler.generateBadRequestException("Total amount does not meet minimum amount");
+            await this.errorHandler.generateBadRequestException("Amount does not meet minimum amount");
         }
         var data = {
             amount: amount,
