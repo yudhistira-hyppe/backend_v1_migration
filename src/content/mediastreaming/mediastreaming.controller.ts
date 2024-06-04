4ae28175dcc04fdd00363c5fa4ee9a8b647d5c13
sukmahyppe <sukma@hyppe.id>
2024-06-04 10:02:57 +0700

    update

--- src/content/mediastreaming/mediastreaming.controller.ts
+++ src/content/mediastreaming/mediastreaming.controller.ts
@@ -343,7 +343,8 @@ export class MediastreamingController {
       );
     }
 
-    const ceckId = await this.mediastreamingService.findOneStreaming(MediastreamingDto_._id.toString());
+    //const ceckId = await this.mediastreamingService.findOneStreaming(MediastreamingDto_._id.toString());
+    const ceckId = await this.mediastreamingService.findOneStreaming2(MediastreamingDto_._id.toString());
     let UserBanned = false;
     let UserReport = false;
     let UserKick = false;
