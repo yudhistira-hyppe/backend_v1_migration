b0f24ff1b387c4f607c464b5f095a0331d50b503
haris-dwi-prakoso <harisdwi.prakoso@gmail.com>
2024-06-10 13:26:07 +0700

    coin transaction history comment out description title & content
    Signed-off-by: haris-dwi-prakoso <harisdwi.prakoso@gmail.com>

--- src/trans/transactions/transactions.controller.ts
+++ src/trans/transactions/transactions.controller.ts
@@ -21741,82 +21741,82 @@ export class TransactionsController {
                             }
                         }
                     }
-                    switch (x.coa) {
-                        case "Paket Coin":
-                            x.desc_title_id = x.package ? `Pembelian Coins ${x.package}` : `Pembelian Coins`;
-                            x.desc_title_en = x.package ? `${x.package} Coins Purchase` : `Coins Purchase`;
-                            x.desc_content_id = `Rp${x.detail[0].amount} dikonversikan dengan ${x.coin} Coins`;
-                            x.desc_content_en = `Rp${x.detail[0].amount} converted to ${x.coin} Coins`;
-                            break;
-                        case "Live Gift":
-                            if (x.coaDetailStatus == "debit") {
-                                x.desc_title_id = `Penerimaan Gift dari Live`;
-                                x.desc_title_en = `Gift received from Live`;
-                                x.desc_content_id = `Gift senilai ${x.coin} Coins diterima dari Live`;
-                                x.desc_content_en = `Gift worth ${x.coin} Coins received from Live`;
-                            } else {
-                                x.desc_title_id = `Pembelian Gift untuk Live`;
-                                x.desc_title_en = `Gift Purchase for Live`;
-                                x.desc_content_id = `Pembelian Gift senilai ${x.coin} Coins`;
-                                x.desc_content_en = `Gift worth ${x.coin} Coins purchased`;
-                            }
-                            break;
-                        case "Content Gift":
-                            if (x.coaDetailStatus == "debit") {
-                                x.desc_title_id = `Penerimaan Gift dari Konten`;
-                                x.desc_title_en = `Gift received from Content`;
-                                x.desc_content_id = `Gift senilai ${x.coin} Coins diterima dari Konten`;
-                                x.desc_content_en = `Gift worth ${x.coin} Coins received from Content`;
-                            } else {
-                                x.desc_title_id = `Pembelian Gift untuk Content`;
-                                x.desc_title_en = `Gift Purchase for Content`;
-                                x.desc_content_id = `Pembelian Gift senilai ${x.coin} Coins`;
-                                x.desc_content_en = `Gift worth ${x.coin} Coins purchased`;
-                            }
-                            break;
-                        case "Penjualan Konten":
-                            x.desc_title_id = `Penerimaan Hasil Penjualan Konten`;
-                            x.desc_title_en = `Content Sales Proceeds`;
-                            x.desc_content_id = `Penjualan Konten seharga ${x.coin} Coins`;
-                            x.desc_content_en = `Content Sales worth ${x.coin} Coins`;
-                            break;
-                        case "Ads":
-                            x.desc_title_id = `Menonton Iklan`;
-                            x.desc_title_en = `Ads Watched`;
-                            x.desc_content_id = `Dari menonton iklan`;
-                            x.desc_content_en = `From watching ads`;
-                            break;
-                        case "Pembelian Konten":
-                            x.desc_title_id = `Pembelian Konten`;
-                            x.desc_title_en = `Content Purchase`;
-                            x.desc_content_id = `Pembelian Konten seharga ${x.coin} Coins`;
-                            x.desc_content_en = `Content Purchase worth ${x.coin} Coins`;
-                            break;
-                        case "Conten Ownership":
-                            x.desc_title_id = `Kepemilikan Konten`;
-                            x.desc_title_en = `Content Ownership`;
-                            x.desc_content_id = `Pembelian Kepemilikan Konten seharga ${x.coin} Coins`;
-                            x.desc_content_en = `Content Ownership Purchase worth ${x.coin} Coins`;
-                            break;
-                        case "Paket Kredit":
-                            x.desc_title_id = x.package ? `Pembelian Credits ${x.package}` : `Pembelian Credits`;
-                            x.desc_title_en = x.package ? `${x.package} Credits Purchase` : `Credits Purchase`;
-                            x.desc_content_id = `Pembelian ${x.detail[0].credit} Credits seharga ${x.coin} Coins`;
-                            x.desc_content_en = `${x.detail[0].credit} Credits Purchase worth ${x.coin} Coins`;
-                            break;
-                        case "Boost Post":
-                            x.desc_title_id = `Boost Postingan`;
-                            x.desc_title_en = `Boost Post`;
-                            x.desc_content_id = `Boost Postingan seharga ${x.coin} Coins`;
-                            x.desc_content_en = `Post Boosted for ${x.coin} Coins`;
-                            break;
-                        case "WD":
-                            x.desc_title_id = `Penukaran Coins`;
-                            x.desc_title_en = `Coins Exchanged`;
-                            x.desc_content_id = `${x.coin} Coins ditukarkan menjadi Rp${x.detail[0].totalAmount}`;
-                            x.desc_content_en = `${x.coin} Coins exchanged to Rp${x.detail[0].totalAmount}`;
-                            break;
-                    }
+                    // switch (x.coa) {
+                    //     case "Paket Coin":
+                    //         x.desc_title_id = x.package ? `Pembelian Coins ${x.package}` : `Pembelian Coins`;
+                    //         x.desc_title_en = x.package ? `${x.package} Coins Purchase` : `Coins Purchase`;
+                    //         x.desc_content_id = `Rp${x.detail[0].amount} dikonversikan dengan ${x.coin} Coins`;
+                    //         x.desc_content_en = `Rp${x.detail[0].amount} converted to ${x.coin} Coins`;
+                    //         break;
+                    //     case "Live Gift":
+                    //         if (x.coaDetailStatus == "debit") {
+                    //             x.desc_title_id = `Penerimaan Gift dari Live`;
+                    //             x.desc_title_en = `Gift received from Live`;
+                    //             x.desc_content_id = `Gift senilai ${x.coin} Coins diterima dari Live`;
+                    //             x.desc_content_en = `Gift worth ${x.coin} Coins received from Live`;
+                    //         } else {
+                    //             x.desc_title_id = `Pembelian Gift untuk Live`;
+                    //             x.desc_title_en = `Gift Purchase for Live`;
+                    //             x.desc_content_id = `Pembelian Gift senilai ${x.coin} Coins`;
+                    //             x.desc_content_en = `Gift worth ${x.coin} Coins purchased`;
+                    //         }
+                    //         break;
+                    //     case "Content Gift":
+                    //         if (x.coaDetailStatus == "debit") {
+                    //             x.desc_title_id = `Penerimaan Gift dari Konten`;
+                    //             x.desc_title_en = `Gift received from Content`;
+                    //             x.desc_content_id = `Gift senilai ${x.coin} Coins diterima dari Konten`;
+                    //             x.desc_content_en = `Gift worth ${x.coin} Coins received from Content`;
+                    //         } else {
+                    //             x.desc_title_id = `Pembelian Gift untuk Content`;
+                    //             x.desc_title_en = `Gift Purchase for Content`;
+                    //             x.desc_content_id = `Pembelian Gift senilai ${x.coin} Coins`;
+                    //             x.desc_content_en = `Gift worth ${x.coin} Coins purchased`;
+                    //         }
+                    //         break;
+                    //     case "Penjualan Konten":
+                    //         x.desc_title_id = `Penerimaan Hasil Penjualan Konten`;
+                    //         x.desc_title_en = `Content Sales Proceeds`;
+                    //         x.desc_content_id = `Penjualan Konten seharga ${x.coin} Coins`;
+                    //         x.desc_content_en = `Content Sales worth ${x.coin} Coins`;
+                    //         break;
+                    //     case "Ads":
+                    //         x.desc_title_id = `Menonton Iklan`;
+                    //         x.desc_title_en = `Ads Watched`;
+                    //         x.desc_content_id = `Dari menonton iklan`;
+                    //         x.desc_content_en = `From watching ads`;
+                    //         break;
+                    //     case "Pembelian Konten":
+                    //         x.desc_title_id = `Pembelian Konten`;
+                    //         x.desc_title_en = `Content Purchase`;
+                    //         x.desc_content_id = `Pembelian Konten seharga ${x.coin} Coins`;
+                    //         x.desc_content_en = `Content Purchase worth ${x.coin} Coins`;
+                    //         break;
+                    //     case "Conten Ownership":
+                    //         x.desc_title_id = `Kepemilikan Konten`;
+                    //         x.desc_title_en = `Content Ownership`;
+                    //         x.desc_content_id = `Pembelian Kepemilikan Konten seharga ${x.coin} Coins`;
+                    //         x.desc_content_en = `Content Ownership Purchase worth ${x.coin} Coins`;
+                    //         break;
+                    //     case "Paket Kredit":
+                    //         x.desc_title_id = x.package ? `Pembelian Credits ${x.package}` : `Pembelian Credits`;
+                    //         x.desc_title_en = x.package ? `${x.package} Credits Purchase` : `Credits Purchase`;
+                    //         x.desc_content_id = `Pembelian ${x.detail[0].credit} Credits seharga ${x.coin} Coins`;
+                    //         x.desc_content_en = `${x.detail[0].credit} Credits Purchase worth ${x.coin} Coins`;
+                    //         break;
+                    //     case "Boost Post":
+                    //         x.desc_title_id = `Boost Postingan`;
+                    //         x.desc_title_en = `Boost Post`;
+                    //         x.desc_content_id = `Boost Postingan seharga ${x.coin} Coins`;
+                    //         x.desc_content_en = `Post Boosted for ${x.coin} Coins`;
+                    //         break;
+                    //     case "WD":
+                    //         x.desc_title_id = `Penukaran Coins`;
+                    //         x.desc_title_en = `Coins Exchanged`;
+                    //         x.desc_content_id = `${x.coin} Coins ditukarkan menjadi Rp${x.detail[0].totalAmount}`;
+                    //         x.desc_content_en = `${x.coin} Coins exchanged to Rp${x.detail[0].totalAmount}`;
+                    //         break;
+                    // }
                 }
             }
             var timestamps_end = await this.utilsService.getDateTimeString();
