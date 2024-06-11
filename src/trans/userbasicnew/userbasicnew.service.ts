ca121ad19cc6bbc5a6f26cf2951e67ea0bc25b72
haris-dwi-prakoso <harisdwi.prakoso@gmail.com>
2024-06-11 10:54:58 +0700

    cointransactionhistory exclude ads clicked
    Signed-off-by: haris-dwi-prakoso <harisdwi.prakoso@gmail.com>

--- src/trans/userbasicnew/userbasicnew.service.ts
+++ src/trans/userbasicnew/userbasicnew.service.ts
@@ -8236,6 +8236,11 @@ export class UserbasicnewService {
                 category: {
                     $nin: [new Types.ObjectId("66306d4dff1a0000750077e2"), new Types.ObjectId("662731cc56375e3a6b2230a6")]
                 }
+            },
+            {
+                product: {
+                    $ne: new Types.ObjectId("660f7d90c306d245ed2c206c")
+                }
             }
         );
         if (status && status.length > 0) matchAnd.push({
