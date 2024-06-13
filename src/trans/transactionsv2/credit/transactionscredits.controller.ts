90209bd0b36fda29f29aa729749e48d18ff5b4e4
hipSalman <salman@hyppe.id>
2024-06-13 15:32:53 +0700

    api listing history credit

--- src/trans/transactionsv2/credit/transactionscredits.controller.ts
+++ src/trans/transactionsv2/credit/transactionscredits.controller.ts
@@ -1,7 +1,90 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Req, UseGuards, Headers, Param } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
+import { UtilsService } from 'src/utils/utils.service';
+import { LogapisService } from 'src/trans/logapis/logapis.service';
+import { ErrorHandler } from 'src/utils/error.handler';
+import { TransactionsCreditsService } from './transactionscredits.service';
 
 @Controller('api/transactions/v2/credit')
 export class TransactionsCreditsController {
     constructor(
+        private readonly utilsService: UtilsService,
+        private readonly logapiSS: LogapisService,
+        private readonly error: ErrorHandler,
+        private readonly transCredit: TransactionsCreditsService,
     ) { }
+
+    @Post('/listing_detail/:id')
+    @UseGuards(JwtAuthGuard)
+    async listing_detail_credit(@Param('id') id:string, @Req() request, @Headers() headers) {
+        var timestamps_start = await this.utilsService.getDateTimeString();
+        var fullurl = headers.host + request.originalUrl;
+        var token = headers['x-auth-token'];
+        var auth = JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
+        var email = auth.email;
+        var request_json = JSON.parse(JSON.stringify(request.body));
+
+        var setid = id;
+        var page = null;
+        var limit = null;
+        var order = null;
+        var startdate = null;
+        var enddate = null;
+        var checkdiscount = null;
+        var transactionid = null;
+        var name = null;
+
+        if(request_json.page == null || request_json.page == undefined) 
+        {
+            await this.error.generateBadRequestException("page field is required");
+        }
+
+        if(request_json.limit == null || request_json.limit == undefined) 
+        {
+            await this.error.generateBadRequestException("limit field is required");
+        }
+
+        if(request_json.descending == null || request_json.descending == undefined) 
+        {
+            await this.error.generateBadRequestException("descending field is required");
+        }
+
+        page = request_json.page;
+        limit = request_json.limit;
+        order = request_json.descending;
+
+        if(request_json.startdate != null && request_json.startdate != undefined && request_json.enddate != null && request_json.enddate != undefined)
+        {
+            startdate = request_json.startdate;
+            enddate = request_json.enddate;
+        }
+
+        if(request_json.checkdiscount != null && request_json.checkdiscount != undefined)
+        {
+            checkdiscount = request_json.checkdiscount;    
+        }
+
+        if(request_json.transactionname != null && request_json.transactionname != undefined)
+        {
+            transactionid = request_json.transactionname;    
+        }
+
+        if(request_json.name != null && request_json.name != undefined)
+        {
+            name = request_json.name;    
+        }
+
+        var data = await this.transCredit.listingDetail(setid, name, transactionid, checkdiscount, startdate, enddate, order, page, limit);
+        const messages = {
+            "info": ["The process was successful"],
+        };
+
+        var timestamps_end = await this.utilsService.getDateTimeString();
+        this.logapiSS.create2(fullurl, timestamps_start, timestamps_end, email, null, null, request_json);
+        return {
+            response_code: 202,
+            data,
+            messages
+        }
+    }
 }
--- src/trans/transactionsv2/credit/transactionscredits.service.ts
+++ src/trans/transactionsv2/credit/transactionscredits.service.ts
@@ -38,4 +38,318 @@ export class TransactionsCreditsService {
     async findByuser(idUser: string): Promise<TransactionsCredits> {
         return await this.transactionsCreditsModel.findOne({ idUser: new mongoose.Types.ObjectId(idUser) }).limit(1).skip(0).sort({ createdAt:-1 }).exec();
     }
+
+    async listingDetail(idtraget:string, name:string, transaction:string, checkdiscount:boolean, startdate:string, enddate:string, order:boolean, page:number, limit:number)
+    {
+        var pipeline = [];
+
+        pipeline.push(
+            {
+                "$match":
+                {
+                    "detail.paketID": idtraget
+                }
+            },
+            {
+                "$lookup":
+                {
+                    from:"monetize",
+                    let:
+                    {
+                        listVoucher:"$voucherDiskon"
+                    },
+                    as:"datadiskon",
+                    pipeline:
+                    [
+                        {
+                            "$match":
+                            {
+                                "$expr":
+                                {
+                                    "$in":
+                                    [
+                                        "$_id",
+                                        "$$listVoucher"
+                                    ]
+                                }
+                            }
+                        },
+                        {
+                            "$project":
+                            {
+                                _id:1,
+                                name:1
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                "$lookup":
+                {
+                    from:"newUserBasics",
+                    localField:"idUser",
+                    foreignField:"_id",
+                    as:"datauser"
+                }
+            },
+            {
+                "$lookup":
+                {
+                    from:"transactionsV2",
+                    localField:"noInvoice",
+                    foreignField:"noInvoice",
+                    as:"datatransaksi"
+                }
+            },
+            {
+                "$project":
+                {
+                    _id:
+                    {
+                        "$arrayElemAt":
+                        [
+                            "$datatransaksi._id", 0
+                        ]
+                    },
+                    createdAt:1,
+                    updatedAt:1,
+                    idTransaction:
+                    {
+                        "$arrayElemAt":
+                        [
+                            "$datatransaksi.idTransaction", 0
+                        ]
+                    },
+                    noInvoice:
+                    {
+                        "$arrayElemAt":
+                        [
+                            "$datatransaksi.noInvoice", 0
+                        ]
+                    },
+                    username:
+                    {
+                        "$arrayElemAt":
+                        [
+                            "$datauser.username", 0
+                        ]
+                    },
+                    fullName:
+                    {
+                        "$arrayElemAt":
+                        [
+                            "$datauser.fullName", 0
+                        ]
+                    },
+                    email:
+                    {
+                        "$arrayElemAt":
+                        [
+                            "$datauser.email", 0
+                        ]
+                    },
+                    avatar: 
+                    {
+                        mediaBasePath: 
+                        {
+                            "$ifNull":
+                            [
+                                {
+                                    "$arrayElemAt":
+                                    [
+                                        "$datauser.mediaBasePath", 0
+                                    ]
+                                },
+                                null
+                            ]
+                        },
+                        mediaUri: 
+                        {
+                            "$ifNull":
+                            [
+                                {
+                                    "$arrayElemAt":
+                                    [
+                                        "$datauser.mediaUri", 0
+                                    ]
+                                },
+                                null
+                            ]
+                        },
+                        mediaType: 
+                        {
+                            "$ifNull":
+                            [
+                                {
+                                    "$arrayElemAt":
+                                    [
+                                        "$datauser.mediaType", 0
+                                    ]
+                                },
+                                null
+                            ]
+                        },
+                        mediaEndpoint: 
+                        {
+                            "$ifNull":
+                            [
+                                {
+                                    "$arrayElemAt":
+                                    [
+                                        "$datauser.mediaEndpoint", 0
+                                    ]
+                                },
+                                null
+                            ]
+                        }
+                    },
+                    using_discount:
+                    {
+                        "$cond":
+                        {
+                            "if":
+                            {
+                                "$ne":
+                                [
+                                    {
+                                        "$size":"$voucherDiskon"
+                                    },
+                                    0
+                                ]
+                            },
+                            "then":true,
+                            "else":false
+                        }
+                    },
+                    totalCredit:
+                    {
+                        "$arrayElemAt":
+                        [
+                            "$detail.amount", 0
+                        ]
+                    },
+                    totalDiscount:
+                    {
+                        "$arrayElemAt":
+                        [
+                            "$detail.discountCoin", 0
+                        ]
+                    },
+                    total:
+                    {
+                        "$arrayElemAt":
+                        [
+                            "$detail.totalAmount", 0
+                        ]
+                    },
+                }
+            }
+        );
+
+        var matchand = [];
+        if(name != null)
+        {
+            matchand.push(
+                {
+                    "$or":
+                    [
+                        {
+                            "username":
+                            {
+                                "$regex":name,
+                                "$options":"i"
+                            }
+                        },
+                        {
+                            "fullName":
+                            {
+                                "$regex":name,
+                                "$options":"i"
+                            }
+                        },
+                        {
+                            "email":
+                            {
+                                "$regex":name,
+                                "$options":"i"
+                            }
+                        },
+                    ]
+                }
+            );    
+        }
+
+        if(transaction != null)
+        {
+            matchand.push(
+                {
+                    "noInvoice":
+                    {
+                        "$regex":transaction,
+                        "$options":"i"
+                    }
+                }
+            );
+        }
+
+        if(checkdiscount != null)
+        {
+            matchand.push(
+                {
+                    "using_discount":checkdiscount
+                }
+            )    
+        }
+
+        if(startdate != null && enddate != null)
+        {
+            try {
+                var currentdate = new Date(new Date(enddate).setDate(new Date(enddate).getDate() + 1));
+                var dateend = currentdate.toISOString().split("T")[0];
+            } catch (e) {
+                dateend = enddate.substring(0,10);
+            }
+
+            matchand.push(
+                {
+                    "createdAt":
+                    {
+                        "$gte":startdate,
+                        "$gt":dateend
+                    }
+                }
+            );
+        }
+
+        if(matchand.length != 0)
+        {
+            pipeline.push(
+                {
+                    "$match":
+                    {
+                        "$and":matchand
+                    }
+                }
+            );    
+        }
+
+        pipeline.push(
+            {
+                "$sort":
+                {
+                    "createdAt":(order == true ? -1 : 1)
+                }
+            },
+            {
+                "$skip":page
+            },
+            {
+                "$limit":limit
+            }
+        );
+
+        var data = await this.transactionsCreditsModel.aggregate(pipeline);
+        return data;
+    }
 }
