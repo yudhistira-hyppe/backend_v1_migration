7f106f4d5a6a3525f5c7124d6c2e5f3c66840e66
Bernardus Yoga <bernarduuuus@gmail.com>
2024-11-23 11:06:41 +0700

    continue describeliveonline list on cloudstreaming.service.ts

--- src/stream/tencent/cloudstreming.service.ts
+++ src/stream/tencent/cloudstreming.service.ts
@@ -45,7 +45,10 @@ export class CloudStreamingService {
         return hash.digest('hex');
     }
 
-    sha256(message: string, secret: string = "", encoding: crypto.BinaryToTextEncoding) {
+    // ini gua kagak tau encoding defaultnya dia mau apa coba cek yang di line 131 itu dia mau pake default encoding.
+    // di docsnya kagak ada ini dia mau default encodingnya apa. kalo ngikutin yang di docs dia error yang 131nya karena kagak ada dfault encoding
+    // https://console.intl.cloud.tencent.com/api/explorer?Product=live&Version=2018-08-01&Action=DescribeLiveStreamOnlineList
+    sha256(message: string, secret: string = "", encoding: crypto.BinaryToTextEncoding = "hex") {
         const hmac = crypto.createHmac("sha256", secret)
         return hmac.update(message).digest(encoding)
       }
@@ -71,15 +74,114 @@ export class CloudStreamingService {
         return unixTimestampExpDateInSeconds.toString(16).toUpperCase();
     }
 
-    // async describeLiveOnlineList(pageNum: number, pageSize: number) {
-    //     try {
-    //         const domainName = process.env.CSS_PUSH_LIVE_DOMAIN
-    //         const appName = process.env.CSS_LIVE_APP_NAME
-    //         const timestamp = Date.now()
-    //         const nonce = 10
-    //         const secretId = 
-    //     } catch (error) {
+    getDate(timestamp: number) {
+        const date = new Date(timestamp * 1000)
+        const year = date.getUTCFullYear()
+        const month = ("0" + (date.getUTCMonth() + 1)).slice(-2)
+        const day = ("0" + date.getUTCDate()).slice(-2)
+        return `${year}-${month}-${day}`
+    }
+
+    async describeLiveOnlineList(pageNum: number, pageSize: number) {
+        try {
+            const secretId = process.env.CSS_SECRET_ID
+            const secretKey = process.env.CSS_SECRET_KEY
+            const token = ""
+            const region = ""
+            const host = process.env.CSS_HOST
+            const service = "live"
+            const action = "DescribeLiveStreamOnlineList"
+            const version = "2018-08-01"
+            const timestamp = parseInt(String(new Date().getTime() / 1000))
+            const date = this.getDate(timestamp)
+            const domainName = process.env.CSS_PUSH_LIVE_DOMAIN
+            const appName = process.env.CSS_LIVE_APP_NAME
+            const nonce = 10
+            const payload = {
+                "DomainName": domainName,
+                "AppName": appName,
+                "PageNum": pageNum,
+                "PageSize": pageSize
+            }
+
+            const stringifyPayload = JSON.stringify(payload)
+
+            // step 1 Concatenate the CanonicalRequest string
+            const signedHeaders = "content-type;host"
+            const hashedRequestPayload = this.getHash(stringifyPayload)
+            const httpRequestMethod = "POST"
+            const canonicalUri = "/"
+            const canonicalQueryString = ""
+            const canonicalHeaders =
+            "content-type:application/json; charset=utf-8\n" + "host:" + host + "\n"
+
+            const canonicalRequest =
+            httpRequestMethod +
+            "\n" +
+            canonicalUri +
+            "\n" +
+            canonicalQueryString +
+            "\n" +
+            canonicalHeaders +
+            "\n" +
+            signedHeaders +
+            "\n" +
+            hashedRequestPayload
+
+            // Step 2: Concatenate the string to sign
+            const algorithm = "TC3-HMAC-SHA256"
+            const hashedCanonicalRequest = this.getHash(canonicalRequest)
+            const credentialScope = date + "/" + service + "/" + "tc3_request"
+            const stringToSign =
+            algorithm +
+            "\n" +
+            timestamp +
+            "\n" +
+            credentialScope +
+            "\n" +
+            hashedCanonicalRequest
+
+            // Step 3: Calculate the Signature
+            const kDate = this.sha256(date, "TC3" + secretKey)
+            const kService = this.sha256(service, kDate)
+            const kSigning = this.sha256("tc3_request", kService)
+            const signature = this.sha256(stringToSign, kSigning, "hex")
+
+            // Step 4: Concatenate the Authorization
+            const authorization =
+            algorithm +
+            " " +
+            "Credential=" +
+            secretId +
+            "/" +
+            credentialScope +
+            ", " +
+            "SignedHeaders=" +
+            signedHeaders +
+            ", " +
+            "Signature=" +
+            signature
+
+            // Step 5: build and send request
+            const headers = {
+                Authorization: authorization,
+                "Content-Type": "application/json; charset=utf-8",
+                Host: host,
+                "X-TC-Action": action,
+                "X-TC-Timestamp": timestamp,
+                "X-TC-Version": version,
+            }
             
-    //     }
-    // }
+            if (region) {
+                headers["X-TC-Region"] = region
+            }
+            if (token) {
+                headers["X-TC-Token"] = token
+            }
+
+            // tinggal buat callnya
+        } catch (error) {
+            throw(error)
+        }
+    }
 }
